5be7ef43e3e80f0047f45ec9130db2ebdb2ecd89
vanchanhnguyen95 <vanchanhnguyen95@gmail.com>
2018-05-24 17:36:30 +0700

    fourth commit: truyen tham so, get, post qua the form,razor

---
+++ DemoBai4/DemoBai4/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoBai4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Hello(string HoTen)
+        {
+            ViewBag.HoTen = HoTen;
+            return View();
+        }
+
+
+
+    }
+}
---
+++ DemoBai4/DemoBai4/Controllers/ThongTinTaiKhoanController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoBai4.Controllers
+{
+    public class ThongTinTaiKhoanController : Controller
+    {
+        // GET: ThongTinTaiKhoan
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(string HoTen)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThongTinTaiKhoan(FormCollection collection, string HoTen)
+        {
+            ViewBag.HoTen = HoTen;
+            ViewBag.TaiKhoan = collection.Get("txtTenDangNhap").ToString();
+            return View();
+        }
+    }
+}
