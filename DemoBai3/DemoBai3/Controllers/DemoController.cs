a1eaa5ab2e91320268d1565f15a5bd876e1fb78e
vanchanhnguyen95 <vanchanhnguyen95@gmail.com>
2018-05-24 15:39:39 +0700

    third commit: unit study razor

---
+++ DemoBai3/DemoBai3/Controllers/DemoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoBai3.Models;
+
+namespace DemoBai3.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Demo()
+        {
+            //Tạo 1 list về tỉnh thành phố
+            List < TinhThanhPho > lstTTP = new List<TinhThanhPho>();
+            TinhThanhPho ttp;
+            ttp = new TinhThanhPho();
+            ttp.MaTinhThanhPho = 1;
+            ttp.TenTinhThanhPho = "TP Hồ Chí Minh";
+            lstTTP.Add(ttp);
+            ttp = new TinhThanhPho();
+            ttp.MaTinhThanhPho = 2;
+            ttp.TenTinhThanhPho = "Hà Nội";
+            lstTTP.Add(ttp);
+
+            //tạo 1 cái ViewBag
+            ViewBag.TinhThanhPho = new SelectList
+                (lstTTP, "MaTinhThanhPho" , "TenTinhThanhPho");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Demo(FormCollection collection)
+        {
+            string sValue = collection["TheThao"].ToString();
+            return View();
+        }
+
+    }
+}
---
+++ DemoBai3/DemoBai3/Models/TinhThanhPho.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoBai3.Models
+{
+    public class TinhThanhPho
+    {
+        public int MaTinhThanhPho { get; set; }
+        public string TenTinhThanhPho
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
