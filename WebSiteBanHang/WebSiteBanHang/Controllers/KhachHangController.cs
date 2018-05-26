50a2198335d259ae4d1407e2a36efde5d417a7ee
vanchanhnguyen95 <vanchanhnguyen95@gmail.com>
2018-05-26 08:26:35 +0700

    fourth commit: tao model, truy van du lieu thong qua cau lenh

---
+++ WebSiteBanHang/WebSiteBanHang/Controllers/KhachHangController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+
+namespace WebSiteBanHang.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        public ActionResult Index()
+        {
+            //Truy vấn dữ liệu thông qua câu lệnh
+            //Đối tượng lstKH sẽ lấy toàn bộ dữ liệu từ bảng KhachHang
+            var lstKH = from KH in db.KhachHangs select KH;
+            return View(lstKH);
+        }
+        public ActionResult Index1()
+        {
+            var lstKH = from KH in db.KhachHangs select KH;
+            return View(lstKH);
+        }
+    }
+}
